f52bdd41bb13a04b10dd40aa14b730199ce304d2
Jesse Smith <jessezao@gmail.com>
2016-12-23 10:04:24 -0600

    added router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'racing-app',
   template: `
-<header class="container">
-  <h1>{{heading}}</h1>
-</header>
-<my-races></my-races>
-`,
+    <header class="container">
+      <h1>{{heading}}</h1>
+    </header>
+    <router-outlet></router-outlet>
+    `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
